7c1649472f5be2c9a55f98a21a424fce3d3aefa5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-12 10:52:36 -0800

    Cat      Platypus

---
+++ src/CatRunner.java
@@ -0,0 +1,13 @@
+
+public class CatRunner {
+public static void main(String[] args) {
+	Cat milk = new Cat("Tom");
+	milk.meow();
+	milk.printName();
+	for(int i=0 ; i<9; i++) {
+		milk.kill();
+	}
+	                        
+	
+}
+}
---
+++ src/PlatypusRunner.java
@@ -0,0 +1,11 @@
+
+public class PlatypusRunner {
+public static void main(String[] args) {
+Platypus arnold = new Platypus("Arnold");
+arnold.sayHi();
+
+
+
+}
+
+}
