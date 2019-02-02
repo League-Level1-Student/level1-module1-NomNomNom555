1f7f32b4985f911cb9221d055121ac241b77760f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-02 10:45:51 -0800

    Vault

---
+++ src/JamesBond.java
@@ -0,0 +1,13 @@
+
+public class JamesBond {
+int findCode(Vault vinney) {
+	for(int i=0; i<=1000000; i++) {
+		if(vinney.tryCode(i)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+}
+
---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+
+public class Runner {
+public static void main(String[] args) {
+	Vault vinney= new Vault();
+	JamesBond bames= new JamesBond();
+	
+}
+}
